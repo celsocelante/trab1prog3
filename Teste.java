deed5367030f86f028259cfcac684e5d333081cf
Celso Junior <celsojr100@gmail.com>
2014-11-05 00:33:47 -0200

    outro teste

--- Teste.java
+++ Teste.java
@@ -1,7 +1,7 @@
 public class Teste {
   public static void main(String[] args){
     System.out.println("Oi!");
-    System.out.println("marcos bicha");
+    System.out.println("rsrsrs");
     // Isso é um teste
   }
 }
