99c05df44870973a565508869e4b3c371d477378
Celso Junior <celsojr100@gmail.com>
2014-11-05 00:31:27 -0200

    teste de commit

--- Teste.java
+++ Teste.java
@@ -1,7 +1,7 @@
 public class Teste {
   public static void main(String[] args){
     System.out.println("Oi!");
-    System.out.println("teste12");
+    System.out.println("marcos bicha");
     // Isso é um teste
   }
 }
