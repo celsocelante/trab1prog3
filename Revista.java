7d9d4aab36b7f94a65fb55fcb0c14ad7b5e5ea76
emMarcos Prado/em <emmprado@inf.ufes.br/em>
2014-11-18 20:25:00 -0200

    Adicionando classe Revista/Cadastro de pessoas

--- Revista.java
+++ Revista.java
@@ -4,17 +4,15 @@ public class Revista{
 	private Edicao edicao;
 	private Set<Tema> temas;
 	private Set<Colaborador> colaboradores;
-	private Set<Artigo> artigos;
 
 
-	//Revista tem conjuntos de colaboradores, temas e artigos, e uma edição.
+	//Revista tem conjuntos de colaboradores, temas, e uma edição.
 	//A cada leitura de um objeto adiciona-se esse objeto primeiramente a lista
 	//correspondente na Revista
 	public Revista(String nome){
 		this.nome=nome;
 		temas=new HashSet<Tema>();
 		colaboradores=new HashSet<Colaborador>();
-		artigos=new HashSet<Artigo>();
 	}
 
 	public void adicionaTema(Tema tema){
@@ -23,9 +21,5 @@ public class Revista{
 
 	public void adicionaColaborador(Colaborador colaborador){
 		colaboradores.add(colaborador);
-	}
-
-	public void adicionaArtigo(Artigo artigo){
-		artigos.add(artigo);
 	}
 }
