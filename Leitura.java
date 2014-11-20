7d77f86a6b3b9432594feaa369b9efc53680616d
Celso Junior <celsojr100@gmail.com>
2014-11-20 19:13:12 -0200

    Classe Main modularizada

---
+++ Leitura.java
@@ -0,0 +1,40 @@
+public class Leitura {
+	private String[] entradas;
+
+	public Leitura(String[] args){
+		entradas = new String[5];
+
+	    if(args.length != 10){
+	      // Imprime mensagem de erro se a quantidade ideal de parâmetros não estiver sendo respeitada
+	      System.out.println("Erro de I/O");
+	      System.exit(1);
+	    }
+
+	    // Inicializa todas as posições do vetor com 'null' para verificação posterior
+	    for(String e: entradas)
+	    	e = null;
+
+	    // Verifica cada elemento do vetor à procura de flags + argumentos
+	    for(int i = 0; i < args.length; i++){
+	      if(args[i].equals("-e"))
+	        entradas[0] = args[i+1];
+	      if(args[i].equals("-t"))
+	        entradas[1] = args[i+1];
+	      if(args[i].equals("-p"))
+	        entradas[2] = args[i+1];
+	      if(args[i].equals("-a"))
+	        entradas[3] = args[i+1];
+	      if(args[i].equals("-r"))
+	        entradas[4] = args[i+1];
+	    }
+	    // Verifica se algum dos parametros não foi preenchido
+	    if(entradas[0] == null|| entradas[1] == null || entradas[2] == null || entradas[3] == null || entradas[4] == null){
+	      System.out.println("Erro de I/O");
+	      System.exit(1);
+	    }
+	}
+
+	public String[] retornaEntradas(){
+		return entradas;
+	}
+}
--- Main.java
+++ Main.java
@@ -5,44 +5,21 @@ public class Main {
 
     Revista revista = new Revista("EngeSoft");
 
-    String e = "NULL";
-    String t = "NULL";
-    String p = "NULL";
-    String a = "NULL";
-    String r = "NULL";
+    Leitura leitura = new Leitura(args);
+    String[] entradas = leitura.retornaEntradas();
 
-    if(args.length != 10){
-      // Lança exceção se a quantidade ideal de parâmetros não estiver sendo respeitada
-      System.out.println("Argumentos insuficientes");
-    }
-    // Verifica cada elemento do vetor à procura de flags + argumentos
-    for(int i = 0; i < args.length; i++){
-      if(args[i].equals("-e"))
-        e = args[i+1];
-      if(args[i].equals("-t"))
-        t = args[i+1];
-      if(args[i].equals("-p"))
-        p = args[i+1];
-      if(args[i].equals("-a"))
-        a = args[i+1];
-      if(args[i].equals("-r"))
-        r = args[i+1];
-    }
-
-    if(e.equals("NULL") || t.equals("NULL") || p.equals("NULL") || a.equals("NULL") || r.equals("NULL")){
-      //tem arugmentos repetidos
-      System.out.println("Argumentos incorretos");
-    }
-
-    // Instancia os objetos que fazem a leitura
+    
     try {
-      CadastroPessoas pessoas = new CadastroPessoas(p, revista);
-      CadastroTemas temas = new CadastroTemas(t, revista);
-      CadastroEdicao edicao = new CadastroEdicao(e, revista);
-      CadastroArtigos artigos = new CadastroArtigos(a, revista);
-      CadastroRevisoes revisoes = new CadastroRevisoes(r, revista);
-    } catch(FileNotFoundException ex){
-      System.out.println("Algum dos arquivos não existe"); // Tratar esta exceção
+      // Instancia os objetos que fazem a leitura
+      CadastroPessoas pessoas = new CadastroPessoas(entradas[2], revista);
+      CadastroTemas temas = new CadastroTemas(entradas[1], revista);
+      CadastroEdicao edicao = new CadastroEdicao(entradas[0], revista);
+      CadastroArtigos artigos = new CadastroArtigos(entradas[3], revista);
+      CadastroRevisoes revisoes = new CadastroRevisoes(entradas[4], revista);
+
+    } catch(FileNotFoundException ex) { // Caso algum dos arquivos não exista
+      System.out.println("Erro de I/O");
+      System.exit(1); // Encerra a execução do programa
     }
 
 
